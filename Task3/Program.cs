fc9d5758e98a59076cd194799c7845c9aa2503e2
Ekyd <Ekyd777@gmail.com>
2022-04-11 20:56:24 +0300

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Ввдите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввдите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = numberB / numberA;
+if (result == numberA)
+{
+    Console.WriteLine("Да ");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
