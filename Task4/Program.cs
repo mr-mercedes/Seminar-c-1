e9ddc4147d5447e488c5bd923c54f9c7bc8044ad
Ekyd <Ekyd777@gmail.com>
2022-04-11 21:13:51 +0300

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,37 @@
+﻿int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+int f = 6;
+int g = 7;
+Console.Write("Введите число:");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == a)
+{
+    Console.WriteLine("Понедельник ");
+}
+if (day == b)
+{
+    Console.WriteLine("Вторник ");
+}
+if (day == c)
+{
+    Console.WriteLine("Среда ");
+}
+if (day == d)
+{
+    Console.WriteLine("Четверг ");
+}
+if (day == e)
+{
+    Console.WriteLine("Пятница ");
+}
+if (day == f)
+{
+    Console.WriteLine("Суббота ");
+}
+if (day == g)
+{
+    Console.WriteLine("Воскресенье ");
+}
